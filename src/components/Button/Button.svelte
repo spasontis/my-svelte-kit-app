4665a3c565c5dbef69d043b4199990c9a908bec8
spasontis <smxxtdzmail@gmail.com>
2026-03-03 12:34:36 +0700

    chore: inital commit

---
+++ src/components/Button/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let { label } = $props();
+</script>
+
+<button>{label}</button>
+
+<style>
+  button {
+    display: inline-block;
+    border: 1px solid red;
+    color: white;
+    padding: 8px 16px;
+    text-align: center;
+    cursor: pointer;
+  }
+</style>
